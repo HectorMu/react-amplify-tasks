b68863cafe39b0b4464fb5013c33516bc3b6f430
hector.munoz <hector.munoz@mxmart.mx>
2022-08-16 12:59:33 -0500

    Some fixes

--- amplify/backend/function/getTasks/src/index.js
+++ amplify/backend/function/getTasks/src/index.js
@@ -34,19 +34,32 @@ pool.query = promisify(pool.query);
 exports.handler = async (event) => {
   const userSub = event.requestContext.authorizer.claims.sub;
 
-  const userTasks = await pool.query("select * from task where fk_user = ?", [
-    userSub,
-  ]);
-
-  return {
-    statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "*",
-      "Access-Control-Allow-Credentials": true,
-      "Access-Control-Allow-Methods": "GET",
-    },
-    body: JSON.stringify(userTasks),
-  };
+  try {
+    const userTasks = await pool.query("select * from task where fk_user = ?", [
+      userSub,
+    ]);
+    return {
+      statusCode: 200,
+      //  Uncomment below to enable CORS requests
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "GET",
+      },
+      body: JSON.stringify(userTasks),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      //  Uncomment below to enable CORS requests
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "GET",
+      },
+      body: JSON.stringify([]),
+    };
+  }
 };
